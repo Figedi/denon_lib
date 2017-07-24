b761c7ac13b919ba9880c02126d3f4dbe7d9251a
Felix Kaminski <kaminski.felix@gmail.com>
2017-07-24 18:50:17 +0200

    Adding queueId, autoformatting with prettier

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ if (require.main === module) {
   const parseArgs = require('minimist'); // eslint-disable-line
   const args = parseArgs(process.argv.slice(2));
   const client = new DenonClient({ host: args.host, retryConnect: true });
-  const cb = (r) => {
+  const cb = r => {
     if (r.error) {
       console.error(r.error); // eslint-disable-line
       process.exit(1);
